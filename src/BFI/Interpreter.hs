88303d14e59704b5a9e080ff0082470b994e3b5d
ysk_univ <ysk.univ.1007@gmail.com>
2022-08-31 22:17:36 +0900

    [src/BFI/Interpreter.hs] error handler to func

--- src/BFI/Interpreter.hs
+++ src/BFI/Interpreter.hs
@@ -13,6 +13,10 @@ runBFFile path = runProgram =<< readFile path
 
 runProgram :: String -> IO ()
 runProgram prog = do
-  _ <- runBFIAction (runCommands $ parseProgram prog) $
-    \msg -> hPutStrLn stderr $ "Error: " ++ msg
+  _ <- runBFIAction (runCommands $ parseProgram prog) errorHandler
   return ()
+
+--
+
+errorHandler :: String -> IO ()
+errorHandler msg = hPutStrLn stderr $ "Error: " ++ msg
